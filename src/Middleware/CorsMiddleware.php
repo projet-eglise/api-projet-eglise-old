a8d184e011bb67b1a55328242561a2a94cf5f939
projet-eglise <projet-eglise@protonmail.com>
2022-04-04 17:35:24 +0200

    PE-5 - Middleware to manage headers cors, to test.

---
+++ src/Middleware/CorsMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Cake\Http\Client\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Cors middleware
+ */
+class CorsMiddleware implements MiddlewareInterface
+{
+    /**
+     * Process method.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The request.
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler The request handler.
+     * @return \Psr\Http\Message\ResponseInterface A response.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // Uncomment when the url of the front exists.
+        // header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+        // header("Access-Control-Allow-Headers: *");
+        if (strtoupper($request->getMethod()) === 'OPTIONS') {
+            return (new Response([]))->withStatus(200);
+        }
+        return $handler->handle($request);
+    }
+}
